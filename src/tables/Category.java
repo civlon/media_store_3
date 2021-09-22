824cd14a697b9a60edf17f6fcf1d5099d8ca337c
Max <max.maercz98@gmail.com>
2021-09-22 21:50:18 +0200

    Adding association mapping category -> subCategory

--- src/tables/Category.java
+++ src/tables/Category.java
@@ -1,10 +1,15 @@
 package tables;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -24,6 +29,9 @@ public class Category {
 	
 	@Column(name = "super_category", nullable = true)
 	private Integer superCategory;
+	
+	@OneToMany(mappedBy = "superCategory")
+	private List<Category> subCategories = new ArrayList<Category>();
 	
 	public Category() {}
 
@@ -57,7 +65,13 @@ public class Category {
 	public void setSuperCategory(Integer superCategory) {
 		this.superCategory = superCategory;
 	}
-	
-	
+
+	public List<Category> getSubCategories() {
+		return subCategories;
+	}
+
+	public void setSubCategories(List<Category> subCategories) {
+		this.subCategories = subCategories;
+	}	
 
 }
